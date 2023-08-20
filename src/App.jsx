cf25298f272be439405a678878f02b8833622886
Shanmukh459 <saikrrish459@gmail.com>
2023-08-20 00:40:50 -0700

    markup for the body

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
 import React from "react"
 import Header from "./components/Header.jsx"
 import Body from "./components/Body.jsx"
+import travelPlaces from "./assets/data.jsx"
 
 function App() {
+  const dataElements = travelPlaces.map((item) => {
+    return (
+      <Body key={item.title} item={item} />
+    )
+  })
   return (
     <>
       <Header />
-      <Body />
+      <main>
+        {dataElements}
+      </main>
+      
     </>
   )
 }
--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,7 +1,23 @@
 import React from "react"
 
-export default function Body() {
+export default function Body(props) {
     return (
-        <h3>Body!</h3>
+        <div className="main-item">
+            <div>
+                <img className="main-image" src={props.item.imageUrl} />
+            </div>
+            <div>
+                <div className="title-location">
+                    <p>{props.item.location}</p>
+                    <a href={props.item.googleMapsUrl}>view on Google Maps</a>
+                </div>
+                <div>
+                    <h2>{props.item.title}</h2>
+                    <h4>{props.item.startDate}-{props.item.endDate}</h4>
+                    <p>{props.item.description}</p>
+                </div>
+                
+            </div> 
+        </div>
     )
 }
